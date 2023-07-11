ba96ca9005768027564a7d7ce5ea1dddadb54d82
MarieAngeLeslie <marieangeleslie0305@gmail.com>
2023-07-11 15:08:17 +0100

    started configuration is done

---
+++ opinions-backend/src/main/java/com/amald/opinionsbackend/OpinionsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.amald.opinionsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpinionsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpinionsBackendApplication.class, args);
+	}
+
+}
---
+++ opinions-backend/src/test/java/com/amald/opinionsbackend/OpinionsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amald.opinionsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpinionsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
